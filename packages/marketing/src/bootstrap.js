a0296bffb736da86000ec1c29aa5f52ecf880a51
Roberto Ramos <roberto.ramos@redica.com>
2024-04-26 13:38:35 -0600

    initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Mount function to start up the app
+const mount = (el) => {
+  ReactDOM.render(
+    <App />,
+    el
+  );
+};
+// Development
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root');
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+// Export the mount function
+
+export { mount };
