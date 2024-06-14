61968f75976e3d4a82e5ffbde9bbb16f79e2b113
Roberto Ramos <rramos6791@gmail.com>
2024-06-14 10:01:18 -0600

    Customize css classes with style provider

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co"
+});
 
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 }
